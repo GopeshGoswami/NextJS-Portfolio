530ed6a5f1139856a1b6e0e949277450be2a3334
Gopesh Goswami <ggopesh45@gmail.com>
2023-01-25 03:03:08 +0530

    Deleted Imports

--- pages/components/Imports.js
+++
@@ -1,8 +0,0 @@
-export { default as Collaboration } from "./Collaboration";
-export { default as Footer } from "./Footer";
-export { default as Intro } from "./Intro";
-export { default as Navbar } from "./Navbar";
-export { default as Particle } from "./Particle";
-export { default as Portfolio } from "./Portfolio";
-export { default as Skills } from "./Skills";
-export { default as Testimonials } from "./Testimonials";
