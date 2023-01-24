8095777001c5e4a87e662559403f5fd3d4ef2feb
Gopesh Goswami <ggopesh45@gmail.com>
2023-01-24 21:27:51 +0530

    Added the project: Youtube-Clone in the portfolio section

--- pages/components/Portfolio.js
+++ pages/components/Portfolio.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import movieApi from "../../public/Portfolio/Movie-Api.png";
-import pokeApi from "../../public/Portfolio/Poke-API.png";
-import mernTodo from "../../public/Portfolio/Mern-Todo.png";
+import {
+  movieApi,
+  pokeApi,
+  mernTodo,
+  youtubeClone,
+} from "../../public/Portfolio/index";
+// import movieApi from "../../public/Portfolio/Movie-Api.png";
 
 export default function Portfolio() {
   return (
@@ -85,6 +89,35 @@ export default function Portfolio() {
             <p className=" text-white text-md mt-3 w-2/3">
               A full stack responsive todo list, with CRUD (Create, Read, Upload
               and Delete) functionalities.
+            </p>
+          </div>
+        </div>
+        {/* To here */}
+        {/* Here  */}
+        <div className=" relative basis-1/3 flex-1">
+          <Image
+            src={youtubeClone}
+            className=" rounded-lg object-cover"
+            width={"100%"}
+            height={"100%"}
+            alt=""
+            layout="responsive"
+          />
+          <div className=" absolute top-0 left-0 w-full h-full rounded-lg hover:cursor-pointer bg-[#00000051] backdrop-blur-sm flex flex-col items-center justify-center text-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <a
+              href="https://youtube-clone-three-zeta.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" text-teal-300 p-2  text-2xl"
+            >
+              Youtube-Clone
+            </a>
+            <p className=" text-white text-md mt-3 w-2/3">
+              A fully responsive youtube-clone, where you can search for any
+              youtube video or channel and play the video right on this site,
+              using a public API. (Currently working on making it MERN stack,
+              adding login ability, along with comments and various other
+              stuff!)
             </p>
           </div>
         </div>
