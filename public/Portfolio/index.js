a59505dc80a757c26e8edb4459e8873d0399150d
Gopesh Goswami <ggopesh45@gmail.com>
2023-01-25 00:18:31 +0530

    Fixed images not showing

--- public/Portfolio/index.js
+++ public/Portfolio/index.js
@@ -1,4 +1,4 @@
-export { defaulta as movieApi } from "./Movie-Api.png";
+export { default as movieApi } from "./Movie-Api.png";
 export { default as pokeApi } from "./Poke-API.png";
 export { default as mernTodo } from "./Mern-Todo.png";
 export { default as youtubeClone } from "./youtube-clone.png";
